ea4eb939db092f45c8a8a873001daa21ef8e47ca
Vaibhav Matere <vpm00718897@gmail.com>
2024-03-04 17:22:12 +0530

    fetchh BE & display msg on FE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
       <h1>DAY 1</h1>
+      {`from server=> ${message}`}
     </div>
   );
 }
